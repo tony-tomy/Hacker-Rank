8480d2ce805bf9655dbed285672765cc1f06a415
tony-tomy <tony.tomy9@gmail.com>
2020-02-19 20:54:30 +0530

    Create sorted_example.py

---
+++ sorted_example.py
@@ -0,0 +1,10 @@
+marksheet = []
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        marksheet.append([name, score])
+
+
+second_highest = sorted(list(set([marks for name, marks in marksheet])))[1]
+print('\n'.join([a for a,b in sorted(marksheet) if b == second_highest]))
